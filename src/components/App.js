6cbce962c6201eb9fd30fa8683fca073ef6b1a10
Jazelle Cruz <jazellecruz22@gmail.com>
2023-02-05 18:19:28 +0800

    add only one state handler

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,47 @@
 import { useState } from "react";
 
 function App() {
-  const [title, setTitle] = useState(null);
-  const [author, setAuthor] = useState(null);
-  const [description, setDescription] = useState(null);
-  const [category_id, setCategory_id] = useState(null);
-  const [status_id, setStatus_id] = useState(null);
+  const [book, setBook] = useState({
+    title: null,
+    author: null,
+    description: null,
+    category_id: null,
+    status_id: null
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, author, description, category_id, status_id);
+    console.log(book);
+  }
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setBook({
+      ...book,
+      [name]: value
+    })
   }
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <label for="title">Title:</label>
-        <input type="text" value={title}  onChange={(e) => setTitle(e.target.value)} name="title"></input>
+        <input type="text" value={book.title} onChange={(e) => handleChange(e)} name="title"></input>
         <br></br>
         <label for="author">Author:</label>
-        <input type="text" value={author}  onChange={(e) => setAuthor(e.target.value)} name="author"></input>
+        <input type="text"  value={book.author} onChange={(e)=> handleChange(e)} name="author"></input>
         <br></br>
         <label for="description">Description:</label>
-        <input type="text" value={description}  onChange={(e) => setDescription(e.target.value)} name="description"></input>
+        <input type="text" value={book.description}  onChange={(e)=> handleChange(e)} name="description"></input>
         <br></br>
         <label for="category_id">Category</label>
-        <select name="category_id" value={category_id} onChange={(e) => setCategory_id(e.target.value)} >
+        <select name="category_id" value={book.category_id} onChange={(e)=> handleChange(e)} >
           <option value="" disabled selected>Select a category</option>
           <option value="1">Academic</option>
         </select>
         <br></br>
         <label for="status_id">Status</label>
-        <select name="status_id" placeholder="status" onChange={(e) => setStatus_id(e.target.value)} value={status_id}>
+        <select name="status_id" placeholder="status" onChange={(e)=> handleChange(e)} value={book.status_id}>
           <option value="" disabled selected>Select a status</option>
           <option value="1">Finished</option>
         </select>
@@ -43,3 +53,5 @@ function App() {
 }
 
 export default App;
+
+
