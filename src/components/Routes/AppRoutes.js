8facb5f9b81e541b2e0bafdddc59afe2b6d80d35
Jazelle Cruz <jazellecruz22@gmail.com>
2023-02-08 16:40:00 +0800

    change routes

--- src/components/Routes/AppRoutes.js
+++ src/components/Routes/AppRoutes.js
@@ -1,12 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "../Dashboard"
-import Profile from "../Profile"
+import Library from "../Library"
 
 function AppRoutes() {
   return(
     <Routes>
-      <Route path="library/*" element={<Dashboard />}/>
-      <Route path="/profile" element={<Profile />}/>
+      <Route path="dashboard/*" element={<Dashboard />} />
     </Routes>
   );
 }
