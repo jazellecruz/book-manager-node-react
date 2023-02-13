b3d6a9145adafa8e5dc39741d816d73ed216e56c
Jazelle Cruz <jazellecruz22@gmail.com>
2023-02-13 22:05:44 +0800

    add categories routes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 import SideNavbar from "./SideNavbar";
 import Library from "./Library";
 import Categories from "./Categories";
 import Wishlist from './Wishlist';
 import Profile from './Profile';
-import {Routes, Route, Outlet} from "react-router-dom"
-import "../styles/dashboard.css"
+import {Routes, Route, Outlet} from "react-router-dom";
+import { kebabCase } from "../helpers/helpers";
+import "../styles/dashboard.css";
 
 function Dashboard() {
+  const [categoriesList, setCategoriesList] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "http://localhost:8000/library/categories"
+    })
+    .then(res => setCategoriesList([...res.data]))
+    .then(console.log(categoriesList))
+    .catch(err => console.error(err))
+  }, []);
+
   return(
     <div className="dashboard-root">
       <div className="sideNavBar-container"> 
@@ -16,7 +30,12 @@ function Dashboard() {
       <div className="main-content-container">
         <Routes>
           <Route path="library/*" element={<Library/>} />
-          <Route path="categories/:category" element={<Categories />}/>
+          {categoriesList.map(category => {
+            let categoryRoute = `categories/${kebabCase(category.category)}`
+            return (
+              <Route path={categoryRoute} element={<Categories category_id={category.category_id} />}/>
+            );
+          })}
           <Route path="wishlist" element={<Wishlist />}/>
           <Route path="profile" element={<Profile />}/>
         </Routes>
