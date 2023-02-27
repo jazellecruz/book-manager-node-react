a17ec116e45b276ada3bc6db496327773a6b4e5e
Jazelle Cruz <jazellecruz22@gmail.com>
2023-02-27 22:26:23 +0800

    add login route

--- src/components/Routes/AppRoutes.jsx
+++ src/components/Routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate} from "react-router-dom";
 import Dashboard from "../Dashboard"
+import Login from "../Login";
 
 function AppRoutes() {
   return(
     <Routes>
       <Route path="/" element={<Navigate replace to="dashboard/library" />} />
+      <Route path="login" element={<Login />} />
       <Route path="dashboard/*" element={<Dashboard />} />
     </Routes>
   );
