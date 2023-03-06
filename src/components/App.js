6fd30fb4803053a5c3e0aaccdaa4e737fcd7ad03
Jazelle Cruz <jazellecruz22@gmail.com>
2023-03-06 21:14:26 +0800

    verify token before mounting components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,46 @@
+import { useEffect } from "react"
+import { useNavigate} from "react-router-dom";
+import axios from "axios"
+import {destroyToken} from "../utils/utils"
 import AppRoutes from "./Routes/AppRoutes";
-import {BrowserRouter} from "react-router-dom"
 import "../styles/global.css";
 
 function App() {
+  const navigate = useNavigate();
+  
+  const verifyToken = () => {
+
+    axios({
+      method: "post",
+      url: "http://localhost:8000/auth/verify",
+      headers: {
+        "x-access-token": localStorage.getItem("accessToken")
+      }})
+      .then(res => {
+        if (res.data.status === 200) {
+          navigate("/dashboard/library")
+        }
+        
+      })
+      .catch(err => {
+        if(err.status === 401) {
+          destroyToken();
+          navigate("/login")
+        }
+      });
+  }
+
+  useEffect(() => {
+    if(!localStorage.getItem("accessToken") ){
+      navigate("/login");
+    } else {
+      verifyToken();
+    }
+  }, [])
+
   return (
     <>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <AppRoutes />
     </>
   );
 }
