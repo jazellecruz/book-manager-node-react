2915d2407962b4383cf0be1bf7ec94a374e1b5ff
Jazelle Cruz <jazellecruz22@gmail.com>
2023-03-07 19:14:26 +0800

    wrap app in context

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,6 +6,7 @@ import Categories from "./Categories";
 import Wishlist from './Wishlist';
 import Profile from './Profile';
 import ProtectedRoute from './ProtectedRoute';
+import { CategoriesContext } from '../contexts/context';
 import {Routes, Route, Outlet} from "react-router-dom";
 import { kebabCase } from "../helpers/helpers";
 import "../styles/dashboard.css";
@@ -26,31 +27,33 @@ function Dashboard() {
   }, []);
 
   return(
+    <CategoriesContext.Provider value={categoriesList}>
     <div className="dashboard-root">
       <div className="sideNavBar-container"> 
-          <SideNavbar />       
+
+          <SideNavbar />
+
       </div>
       <div className="main-content-container">
         <Routes>
           <Route path="library/*" element={ <ProtectedRoute component={<Library/>} />} />
-          {categoriesList.map(category => {
-            let categoryRoute = `categories/${kebabCase(category.category)}`
-            return (
-              <Route path={categoryRoute} 
-                element={<ProtectedRoute component={<Categories category_id={category.category_id} category={category.category}/>} />} />
-            );
-          })}
+            {categoriesList.map(category => {
+              let categoryRoute = `categories/${kebabCase(category.category)}`
+              return (
+                <Route path={categoryRoute} 
+                  element={<ProtectedRoute component={<Categories category_id={category.category_id} category={category.category}/>} />} />
+              );
+            })}
           <Route path="wishlist" element={<ProtectedRoute component={<Wishlist /> } />}/>
           <Route path="profile" element={<ProtectedRoute component={<Profile /> } />}/>
-        </Routes>
+        </Routes>       
         <Outlet />
       </div>
       <div>
-        {/*components to view goals */}
+      {/*components to view goals */}
       </div>      
     </div>
-
-
+    </CategoriesContext.Provider>
   );
 }
 
