a99dc3fdcd6864675349c33d0e601f7522a98058
Jazelle Cruz <jazellecruz22@gmail.com>
2023-03-08 17:54:59 +0800

    add elements

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios";
 
 export default function Wishlist() {
+  const [books, setBooks] = useState({});
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://localhost:8000/library/books?status_id=4",
+      headers: {
+        "x-access-token": localStorage.getItem("accessToken")
+      }
+    })
+    .then(res => setBooks({...res.data}))
+    .then(console.log(books))
+    .catch(err => console.log(err))
+  }, []);
+
   return (
     <div>
-      This is the wishlist page!!!
+        <div className="category-box">
+          <p>Wishlist</p>
+          <p className="secondary-text">
+          You currently have <span>{books.count} </span> 
+          {books.count === 1 ? "book" : "books"} for <span> Wishlist</span>.</p>
+      </div>
     </div>
   )
 }
