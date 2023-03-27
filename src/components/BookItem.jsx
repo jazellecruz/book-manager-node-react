af33e2e34a69850d57db1ea554311502163c2bde
Jazelle Cruz <jazellecruz22@gmail.com>
2023-03-27 21:15:48 +0800

    add confirm box for delete

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,11 +1,27 @@
-import { Link, } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from 'react-router-dom';
 import axios from "axios";
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-import { Rating } from "@mui/material"
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Rating  from "@mui/material/Rating";
+import Button  from "@mui/material/Button";
 import { kebabCase, trimString } from "../helpers/helpers"
 import "../styles/bookItem.css"
 
 function BookItem({title, author, description, img, rating, category, status, precision, book_id, renderComponent}) {
+  const [openDialogue, setOpenDialogue] = useState(false);
+
+  const handleOpenDialogue = () => {
+    setOpenDialogue(true);
+  };
+
+  const handleCloseDialogue = () => {
+    setOpenDialogue(false);
+  };
 
   const handleDelete = (book_id) => {
     axios({
@@ -48,10 +64,41 @@ function BookItem({title, author, description, img, rating, category, status, pr
           </p>
         </div>
       </div>
-      <button className="deleteBook-btn" onClick={() => handleDelete(book_id)}>
+      <button className="deleteBook-btn" onClick={handleOpenDialogue}>
           <DeleteOutlineOutlinedIcon />
       </button>
+      <div>
+      <Dialog
+        open={openDialogue}
+        onClose={handleCloseDialogue}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Delete this book?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to 
+            <span style={{color: "red"}}> delete </span>
+            <span style={{fontWeight: "bolder"}}>{`"${title}"`}</span>
+            ?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialogue}>Cancel</Button>
+          <Button onClick={() => {
+            handleDelete(book_id)
+            handleCloseDialogue();
+            }} 
+            autoFocus>
+            YES
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
       </div>
+      
   );
 }
 
