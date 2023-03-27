d0f0458812fdb0e60cc21724191cc1f3fca433f1
Jazelle Cruz <jazellecruz22@gmail.com>
2023-03-27 21:19:03 +0800

    remove validateToken func

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,6 +5,7 @@ const isUserAuthenticated = () => {
   return localStorage.getItem("accessToken") !== null
 }
 
+
 const saveToken = (token) => {
   localStorage.setItem("accessToken", token)
 }
@@ -13,28 +14,4 @@ const destroyToken = () => {
   localStorage.removeItem("accessToken")
 }
 
-
-const validateToken = () => {
-  let token = localStorage.getItem("accessToken")
-
-  if(!token) return false;
-
-  axios({
-    method: "post",
-    url: "http://localhost:8000/auth/verify",
-    headers: {
-      "x-access-token": token
-    }})
-    .then(res => {
-      if (res.data.status === 200) {
-        return true
-      }
-    })
-    .catch(err => {
-      localStorage.setItem("user", {isUserAuthenticated: false});
-    });
-
-}
-
-  // return localStorage.getItem("accessToken") !== null
-export {isUserAuthenticated, saveToken, destroyToken, validateToken}
+export {isUserAuthenticated, saveToken, destroyToken}
