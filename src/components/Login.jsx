5d284ea38614adcfd2b8029454da49efb16afb65
Jazelle Cruz <jazellecruz22@gmail.com>
2023-04-03 22:25:51 +0800

    add snackbar for errors

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,12 +4,24 @@ import axios from 'axios'
 import "../styles/login.css"
 import { saveToken } from "../utils/utils.js"
 import bookedLogo from "../assets/booked.png"
+import {Snackbar, Alert} from "@mui/material";
 
 const Login = () => {
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  const [error, setError] = useState({
+    message: "",
+    status: ""
+  });
+
   const [credentials, setCredentials] = useState({
     username: "",
     password: ""
   });
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false)
+  };
 
   const navigate = useNavigate();
 
@@ -19,8 +31,8 @@ const Login = () => {
     setCredentials({
       ...credentials,
       [name] : value
-    })
-  }
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,11 +42,23 @@ const Login = () => {
       data: credentials,
     })
     .then(res => {
-      console.log(res)
       saveToken(res.data.accessToken);
       navigate("/");
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      if (err.response.status === 401 || err.response.status === 404) {
+        setError({
+          message: "Invalid user credentials.",
+          status: err.response.status
+        })
+      } else if (err.response.status === 500) {
+        setError({
+          message: "Internal Error in Server. Try again later.",
+          status: err.response.status
+        });
+      }
+      setOpenSnackbar(true);
+    });
 
     setCredentials({
       username: "",
@@ -43,39 +67,47 @@ const Login = () => {
   }
 
   return (
-    <div className="login-section">
-    <img src={bookedLogo} alt="booked-logo"/>
-      <p>Welcome back</p>
-      <p>Please enter your details.</p>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <div>
-          <label for="username">Username</label>
-          <input className="login-input" 
-            type="text" 
-            name="username" 
-            value={credentials.username} 
-            placeholder="Username"
-            autoComplete="off" 
-            onChange={(e) => handleChange(e)}
-            required>
-          </input>
-        </div>
-        <div>
-          <label for="password">Password</label>
-          <input className="login-input" 
-            type="password" 
-            name="password" 
-            value={credentials.password} 
-            placeholder="Password" 
-            autoComplete="off" 
-            onChange={(e) => handleChange(e)}
-            required>
-          </input>
-        </div>
-
-        <button type="submit" className="login-btn login-input">Login</button>
-      </form>
-    </div>
+    <>
+      <div className="login-section">
+        <img src={bookedLogo} alt="booked-logo"/>
+        <p>Welcome back</p>
+        <p>Please enter your details.</p>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <label for="username">Username</label>
+            <input className="login-input" 
+              type="text" 
+              name="username" 
+              value={credentials.username} 
+              placeholder="Username"
+              autoComplete="off" 
+              onChange={(e) => handleChange(e)}
+              required>
+            </input>
+          </div>
+          <div>
+            <label for="password">Password</label>
+            <input className="login-input" 
+              type="password" 
+              name="password" 
+              value={credentials.password} 
+              placeholder="Password" 
+              autoComplete="off" 
+              onChange={(e) => handleChange(e)}
+              required>
+            </input>
+          </div>
+          <button type="submit" className="login-btn login-input">Login</button>
+        </form>
+      </div>
+      {openSnackbar && 
+          <Snackbar open={openSnackbar} autoHideDuration={5000} onClose={handleCloseSnackbar}>
+            <Alert onClose={handleCloseSnackbar} variant="filled" severity="error" sx={{ width: '100%' }}>
+              {`${error.message} (Error ${error.status})`}
+            </Alert>
+          </Snackbar>
+        }
+    </>
   )
 }
 
