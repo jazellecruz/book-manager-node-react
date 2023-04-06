8ed49ff1793fa9a9d516df9172f3c2610cba30f7
Jazelle Cruz <jazellecruz22@gmail.com>
2023-04-06 18:33:17 +0800

    add input for rating and review

--- src/components/FormModal.jsx
+++ src/components/FormModal.jsx
@@ -6,31 +6,31 @@ import { Modal,
          ThemeProvider, 
          Button,
          Alert,
-         Snackbar } from "@mui/material"
+         Snackbar,
+         Rating } from "@mui/material"
 import ControlPointRoundedIcon from '@mui/icons-material/ControlPointRounded';
 import {addBookTheme, modalStyle} from "../styles/themes/themes"
 import { CategoriesContext, SnackbarContext } from "../contexts/context";
 import { sanitizeInput } from "../helpers/helpers"
 import "../styles/form.css"
 
-function FormModal({renderComponent}) {
+function FormModal({setNewBooksList, renderComponent}) {
   const [openBookForm, setOpenBookForm] = useState(false);
-  const [openSnackbar, setOpenSnackbar] = useState(false);
   const [statusList, setStatusList] = useState([])
-  const [snackbarType, setSnackbarType] = useState({
-    message: "",
-    severity: ""
-  });
+  const [rating, setRating] = useState(null);
   const [entry, setEntry] = useState({
     title: null,
     author: null,
     description: null,
     img: null,
     category_id: null,
-    status_id: null
+    status_id: null,
+    rating: rating,
+    review: null
   });
+  
   const categoriesList = useContext(CategoriesContext);
-  const snackbar = useContext(SnackbarContext);
+  const openSnackbar = useContext(SnackbarContext);
 
   const emptyEntry = () => {
     setEntry({
@@ -40,7 +40,8 @@ function FormModal({renderComponent}) {
       img: null,
       category_id: null,
       status_id: null
-    })
+    });
+    setRating(null);
   }
 
   const handleCancel = () => {
@@ -55,7 +56,8 @@ function FormModal({renderComponent}) {
     setEntry({
       ...entry,
       [name] : sanitizeInput(value, name)
-    })
+    });
+    console.log(entry)
   }
 
   const handleSubmit = (e) => {
@@ -74,29 +76,21 @@ function FormModal({renderComponent}) {
         description: entry.description,
         img: entry.img,
         category_id: entry.category_id,
-        status_id: entry.status_id
+        status_id: entry.status_id,
+        rating: entry.rating,
+        review: entry.review
       }
     })
     .then(res => {
       if(res.status === 200) {
-        console.log(res)
+        setNewBooksList(res.data)
         handleCancel();
-        renderComponent();
-        snackbar("success", "Successfully Saved!")
-        // setSnackbarType({
-        //   message:"Successfully saved!",
-        //   severity: "success"
-        // });
-        // setOpenSnackbar(!openSnackbar)
+        openSnackbar("success", "Successfully Saved!")
       }
     })
     .catch(err => {
       handleCancel();
-      snackbar("error", `Request failed! Try again later. (Error ${err.response.status})`)
-      // setSnackbarType({
-      //   message: `Request failed! Try again later. (Error ${err.response.status})`,
-      //   severity: "error"
-      // });
+      openSnackbar("error", `Request failed! Try again later. (Error ${err.response.status})`)
     })
   }
 
@@ -136,14 +130,15 @@ function FormModal({renderComponent}) {
     <Box sx={modalStyle}>
     <h3>Add a new book</h3>
     <form onSubmit={handleSubmit}>
-      <div className="input-container">
-        <label for="title">Title:</label>
-        <input className="input-border" type="text" value={entry.title} onChange={(e) => handleChange(e)} name="title"></input>
-      </div>
-        
-      <div className="input-container">
-        <label for="author">Author:</label>
-        <input className="input-border" type="text"  value={entry.author} onChange={(e)=> handleChange(e)} name="author"></input>
+      <div className="input-group"> 
+        <div className="input-container">
+          <label for="title">Title:</label>
+          <input className="input-border" type="text" value={entry.title} onChange={(e) => handleChange(e)} name="title"></input>
+        </div>
+        <div className="input-container">
+          <label for="author">Author:</label>
+          <input className="input-border" type="text"  value={entry.author} onChange={(e)=> handleChange(e)} name="author"></input>
+        </div>
       </div>
       <div className="input-container">
         <label for="description">Description:</label>
@@ -153,28 +148,45 @@ function FormModal({renderComponent}) {
         <label for="description">Image:</label>
         <input className="input-border" type="text" value={entry.img}  onChange={(e)=> handleChange(e)} name="img"></input>
       </div>
-      <div className="input-container">
-        <label for="category_id">Category:</label>
-          <select className="input-border" name="category_id" value={entry.category_id} onChange={(e)=> handleChange(e)} >
-          <option value="" disabled selected>Select a category</option>
-          <CategoriesContext.Consumer >
-          {categoriesList => 
-            categoriesList.map(({ category, category_id}) =>
-            <option value={category_id}>{category}</option>
-            )
-          }
-          </CategoriesContext.Consumer>
-          </select>
+      <div className="input-group"> 
+        <div className="input-container">
+          <label for="category_id">Category:</label>
+            <select className="input-border" name="category_id" value={entry.category_id} onChange={(e)=> handleChange(e)} >
+              <option value="" disabled selected>Select a category</option>
+              <CategoriesContext.Consumer >
+                {categoriesList => 
+                  categoriesList.map(({ category, category_id}) =>
+                  <option value={category_id}>{category}</option>
+                  )
+                }
+              </CategoriesContext.Consumer>
+            </select>
+        </div>
+        <div className="input-container">
+          <label for="status_id">Status:</label>
+            <select className="input-border" name="status_id" placeholder="status" onChange={(e)=> handleChange(e)} value={entry.status_id}>
+              <option value="" disabled selected>Select a status</option>
+              {statusList.map(({ status_id, status}) =>
+                <option value={status_id}>{status}</option>
+                )
+              }
+            </select>
+        </div>
       </div>
       <div className="input-container">
-        <label for="status_id">Status:</label>
-        <select className="input-border" name="status_id" placeholder="status" onChange={(e)=> handleChange(e)} value={entry.status_id}>
-          <option value="" disabled selected>Select a status</option>
-          {statusList.map(({ status_id, status}) =>
-            <option value={status_id}>{status}</option>
-            )
-          }
-        </select>
+        <label>Rating:</label>
+        <Rating
+          name="rating"
+          value={rating}
+          onChange={(e, newValue) => {
+            setRating(newValue);
+            handleChange(e);
+          }}
+        />
+      </div>
+      <div className="input-container">
+        <label for="review">Review:</label>
+        <textarea className="input-border review" value={entry.review} name="review" onChange={(e)=> handleChange(e)}></textarea>
       </div>
       <div className="form-btn-container">
         <button type="submit" className="submit-btn">Submit</button>
