78f3a1a681522be9dc7af22f4514024f3f8d7fb2
jazzcruz <jazellecruz22@gmail.com>
2023-09-28 20:39:37 +0800

    add default route

--- client/src/components/Routes/AppRoutes.jsx
+++ client/src/components/Routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate,  } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "../ProtectedRoute";
 import Dashboard from "../Dashboard"
 import Library from "../Library";
@@ -8,9 +8,11 @@ import Error from "../Error";
 function AppRoutes() {
   return(
     <Routes>
+      <Route exact path="/" element={<Navigate to="library" />  } />
       <Route path="login" element={<Login />} />
       <Route path="error" element={<Error />} />
       <Route path="library" element={<Library />} />
+
       {/* <Route path="library" element={<Library/>} /> */}
 
       {/* <Route path="/" element={<ProtectedRoute component={<Navigate to="dashboard/library" />} /> } /> */}
